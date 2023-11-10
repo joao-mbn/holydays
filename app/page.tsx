0254f5d023e3b6a26da110839be7a62e7322ce3d
João Nascimento <joaombn97@gmail.com>
2023-11-10 19:49:56 -0300

    Attempt 3: removing some optimizations.

--- app/page.tsx
+++ app/page.tsx
@@ -63,7 +63,7 @@ export default function Home() {
 
   return (
     <main className="relative flex h-screen w-screen flex-col items-center gap-6 overflow-hidden font-sans text-sky-950">
-      <Image alt="Beach Scene" src="/bg-16-9.webp" priority fill className="absolute object-cover" />
+      <Image alt="Beach Scene" src="/bg-16-9.webp" /* priority  */ fill className="absolute object-cover" />
       <div className="z-10 flex h-full w-full flex-col gap-20 bg-white/60 backdrop-brightness-125">
         <header className="mt-10 flex flex-col items-center gap-2 font-display">
           <h1 className="text-8xl">Holydays</h1>
