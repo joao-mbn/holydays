43d8928116cb1ec2a4424ffd81c02f0828294885
João Nascimento <joaombn97@gmail.com>
2025-09-18 18:38:43 -0300

    fixing tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,11 @@
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   test: {
     environment: 'jsdom',
     exclude: ['./e2e/*'],
+    include: ['./app/**/*.test.ts'],
   },
 });
 
